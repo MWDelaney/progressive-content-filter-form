3215da401ce77506c602aaeb59df3e39de584813
Michael W. Delaney <michael.delaney@gmail.com>
2017-06-08 15:31:10 -0400

    Create questions-form.php

--- views/partials/questions-form.php
+++ views/partials/questions-form.php
@@ -1,16 +1,17 @@
 <form class="pcff-form" action="<?php the_permalink(); ?>" method="post">
-<?php do_action('pcff-before-questions-item'); ?>
-	<?php while ( have_rows('pcff_questions') ) : the_row(); ?>
-		<?php
-			$context->template->get_template_part( 'questions', 'item' );
-		?>
-	<?php endwhile; ?>
-<?php do_action('pcff-after-questions-item'); ?>
+	<ul>
+	<?php do_action('pcff-before-questions-item'); ?>
+		<?php while ( have_rows('pcff_questions') ) : the_row(); ?>
+			<?php
+				$context->template->get_template_part( 'questions', 'item' );
+			?>
+		<?php endwhile; ?>
+	<?php do_action('pcff-after-questions-item'); ?>
 
 
-	<div class="pcff-questions-item">
-			<input class="pcff-answers" type="hidden" name="pcff-answers" value="">
-			<button title="Submit" id="pcff-submit" class="btn btn-secondary btn-lg" type="submit"><?php _e('Submit'); ?></button>
-	</div>
-
+		<li class="pcff-questions-item">
+				<input class="pcff-answers" type="hidden" name="pcff-answers" value="">
+				<button title="Submit" id="pcff-submit" class="btn btn-secondary btn-lg" type="submit"><?php _e('Submit'); ?></button>
+		</li>
+	</ul>
 </form>
